4dba9335227bf0f15889b5bbeef2d402f86812c6
Bayram Tatlı <tatlbayram@gmail.com>
2024-01-21 22:50:02 +0300

    Frontend admin panelinde klasörleme değişiklikleri

--- ETicaretProje.FrontEnd/src/app/app.routes.ts
+++ ETicaretProje.FrontEnd/src/app/app.routes.ts
@@ -6,6 +6,11 @@ import { AboutComponent } from './components/shop/about/about.component';
 import { MarketComponent } from './components/shop/market/market.component';
 import { CartComponent } from './components/shop/cart/cart.component';
 import { CheckoutComponent } from './components/shop/checkout/checkout.component';
+import { AdminComponent } from './components/admin/admin/admin.component';
+import { CategorylistComponent } from './components/admin/categorylist/categorylist.component';
+import { AdminLoginComponent } from './components/admin/admin-login/admin-login.component';
+import { AdminRegisterComponent } from './components/admin/admin-register/admin-register.component';
+import { ProductlistComponent } from './components/admin/productlist/productlist.component';
 
 export const routes: Routes = [
     {
@@ -16,6 +21,18 @@ export const routes: Routes = [
             {path:'products', component: MarketComponent},
             {path:'cart', component: CartComponent},
             {path:'checkout', component: CheckoutComponent}
+        ]
+    },
+    {
+        path:'admin/login',component:AdminLoginComponent
+    },
+    {
+        path:'admin/register',component:AdminRegisterComponent
+    },
+    {
+        path:'admin',component:AdminComponent, children:[
+            {path:'categorylist',component:CategorylistComponent},
+            {path:'productlist',component:ProductlistComponent}
         ]
     },
     { path: '', redirectTo: 'shop/index', pathMatch: 'full' },
